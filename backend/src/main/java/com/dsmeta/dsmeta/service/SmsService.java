08cf0d76d8bacde0c515f97233fb2353d5c4f6df
Joanderson Felipe <joandersonfsbarbosa@gmail.com>
2022-09-10 21:03:05 -0300

    SMS system running

---
+++ backend/src/main/java/com/dsmeta/dsmeta/service/SmsService.java
@@ -0,0 +1,45 @@
+package com.dsmeta.dsmeta.service;
+
+import com.dsmeta.dsmeta.entity.Sale;
+import com.dsmeta.dsmeta.repository.SaleRepository;
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SmsService {
+
+    @Value("${twilio.sid}")
+    private String twilioSid;
+
+    @Value("${twilio.key}")
+    private String twilioKey;
+
+    @Value("${twilio.phone.from}")
+    private String twilioPhoneFrom;
+
+    @Value("${twilio.phone.to}")
+    private String twilioPhoneTo;
+
+    @Autowired
+    private SaleRepository saleRepository;
+
+    public void sendSms(Long saleId) {
+
+        Sale sale = saleRepository.findById(saleId).get();
+
+        String msg = "Vendedor " + sale.getSellerName() + " vendeu " + sale.getAmount() + " na data " + sale.getDate();
+
+        Twilio.init(twilioSid, twilioKey);
+
+        PhoneNumber to = new PhoneNumber(twilioPhoneTo);
+        PhoneNumber from = new PhoneNumber(twilioPhoneFrom);
+
+        Message message = Message.creator(to, from, msg).create();
+
+        System.out.println(message.getSid());
+    }
+}
